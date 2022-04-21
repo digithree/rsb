44f08142f209c9d8f1552b667531727e0dcbacf4
Simon Kenny <1c3k1ng@tutanota.com>
2022-04-21 16:02:36 +0100

    Move module information to character creation and store in DB instead of hardcoding

--- bundles/character-creator/index.js
+++ bundles/character-creator/index.js
@@ -66,7 +66,8 @@ module.exports = {
 			uuid: uuid,
 			type: "og-bot",
 			level: 1,
-			playerCharacter: true
+			playerCharacter: true,
+			modules: this.startingModuleStatuses
 		});
 
 		socket.account.characters.push(server.db.getId(character));
@@ -128,5 +129,448 @@ module.exports = {
 		"Score",
 		"Clone",
 		"Deed"
+	],
+
+	"startingModuleStatuses": [
+		{
+			"name": "Radio",
+			"type": "Communications",
+			"description": "Allows for remote control and communication.",
+			"status": "Operational",
+			"active": true,
+			"error": false,
+			"level": 1,
+			"max": 1,
+			"current": 1,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": [],
+			"energy": 3,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 6,
+					y: 7,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Radar",
+			"type": "Sensor",
+			"description": "Sees local area.",
+			"status": "Unknown damage!",
+			"active": false,
+			"error": true,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["scan-local"],
+			"energy": 50,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 5,
+					y: 3,
+					len: 1
+				}
+			]
+		},
+		{
+			"name": "SOS Eye",
+			"type": "Sensor",
+			"description": "Star Orientation System Eye: Determines location in galaxy.",
+			"status": "Unknown damage!",
+			"active": false,
+			"error": true,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["scan-local"],
+			"energy": 10,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 17,
+					y: 3,
+					len: 1
+				}
+			]
+		},
+		{
+			"name": "Memory",
+			"type": "Circuitry",
+			"description": "Onboard computer memory. Capacity limits operations.",
+			"status": "Reduced capacity!",
+			"active": true,
+			"error": false,
+			"level": 1,
+			"max": 1048576, //2^20
+			"current": 128,
+			"valueTerm": "Bytes",
+			"warningBelow": 1024,
+			"warningAbove": 1048576, //2^20
+			"actions": [],
+			"energy": 2,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 6,
+					y: 9,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Legs",
+			"type": "Actuator",
+			"description": "Claw-like legs. Facilitate latching and movement.",
+			"status": "Latched to surface but motor stuck!",
+			"active": false,
+			"error": true,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["crawl", "push", "latch"],
+			"energy": 10,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 0,
+					y: 3,
+					len: 3
+				},
+				{
+					x: 2,
+					y: 4,
+					len: 3
+				},
+				{
+					x: 1,
+					y: 6,
+					len: 4
+				},
+				{
+					x: 1,
+					y: 8,
+					len: 4
+				},
+				{
+					x: 2,
+					y: 10,
+					len: 3
+				},
+				{
+					x: 0,
+					y: 11,
+					len: 3
+				},
+				{
+					x: 20,
+					y: 3,
+					len: 3
+				},
+				{
+					x: 18,
+					y: 4,
+					len: 3
+				},
+				{
+					x: 18,
+					y: 6,
+					len: 4
+				},
+				{
+					x: 18,
+					y: 8,
+					len: 4
+				},
+				{
+					x: 18,
+					y: 10,
+					len: 3
+				},
+				{
+					x: 20,
+					y: 11,
+					len: 3
+				},
+			]
+		},
+		{
+			"name": "Arms",
+			"type": "Actuator",
+			"description": "Clamps on arms. May manipulate close-by objects and materials.",
+			"status": "Operational",
+			"active": false,
+			"error": false,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["harvest", "repair", "build"],
+			"energy": 5,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 6,
+					y: 0,
+					len: 3
+				},
+				{
+					x: 14,
+					y: 0,
+					len: 3
+				},
+				{
+					x: 6,
+					y: 1,
+					len: 1
+				},
+				{
+					x: 16,
+					y: 1,
+					len: 1
+				},
+				{
+					x: 6,
+					y: 2,
+					len: 3
+				},
+				{
+					x: 14,
+					y: 2,
+					len: 3
+				},
+			]
+		},
+		{
+			"name": "Silicon extractor",
+			"type": "Extractor",
+			"description": "Materials processing for silicon, extracting from rocks and debris.",
+			"status": "Operational",
+			"active": false,
+			"error": false,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": [],
+			"energy": 2,
+			"yield": 1,
+			"yieldType": "SIL",
+			"colorize": [
+				{
+					x: 14,
+					y: 5,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Metals extractor",
+			"type": "Extractor",
+			"description": "Materials processing for metals, extracting from rocks and debris.",
+			"status": "Operational",
+			"active": false,
+			"error": false,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": [],
+			"energy": 3,
+			"yield": 2,
+			"yieldType": "MET",
+			"colorize": [
+				{
+					x: 6,
+					y: 5,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Materials storage",
+			"type": "Storage",
+			"description": "General purpose storage for materials.",
+			"status": "Operational",
+			"active": true,
+			"error": false,
+			"level": 1,
+			"max": 40,
+			"current": 0,
+			"valueTerm": "Capacity",
+			"warningBelow": 0,
+			"warningAbove": 35,
+			"actions": [],
+			"energy": 0,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 10,
+					y: 3,
+					len: 3
+				},
+				{
+					x: 10,
+					y: 4,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Module factory",
+			"type": "Factory",
+			"description": "A module which can build other modules.",
+			"status": "Operational",
+			"active": false,
+			"error": false,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["fabricate"],
+			"energy": 40,
+			"yield": 0.4,
+			"yieldType": "MOD",
+			"colorize": [
+				{
+					x: 10,
+					y: 5,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Solar panel",
+			"type": "Energy",
+			"description": "Energy harvesting from light energy.",
+			"status": "Panel damage!",
+			"active": true,
+			"error": false,
+			"level": 1,
+			"max": 100,
+			"current": 5,
+			"valueTerm": "Energy output",
+			"warningBelow": 20,
+			"warningAbove": 100,
+			"actions": [],
+			"energy": 0,
+			"yield": 6,
+			"yieldType": "NRG",
+			"colorize": [
+				{
+					x: 6,
+					y: 10,
+					len: 11
+				}
+			]
+		},
+		{
+			"name": "Battery",
+			"type": "Energy",
+			"description": "Energy storage for bot computer, sensors and actuators.",
+			"status": "Low capacity!",
+			"active": true,
+			"error": false,
+			"level": 1,
+			"max": 20,
+			"current": 20,
+			"valueTerm": "Stored",
+			"warningBelow": 50,
+			"warningAbove": 100,
+			"actions": [],
+			"energy": -10,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 14,
+					y: 9,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Fuel",
+			"type": "Energy",
+			"description": "Fuel storage for engines.",
+			"status": "Operational",
+			"active": false,
+			"error": false,
+			"level": 1,
+			"max": 100,
+			"current": 0,
+			"valueTerm": "Stored",
+			"warningBelow": 40,
+			"warningAbove": 100,
+			"actions": [],
+			"energy": 0,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 10,
+					y: 9,
+					len: 3
+				}
+			]
+		},
+		{
+			"name": "Ion Engine",
+			"type": "Engine",
+			"description": "Propulsion engine for slow, long journeys.",
+			"status": "Unknown damage!",
+			"active": false,
+			"error": true,
+			"level": 1,
+			"max": 1,
+			"current": 0,
+			"valueTerm": "Thrust",
+			"warningBelow": 0,
+			"warningAbove": 1,
+			"actions": ["fly"],
+			"energy": 1000,
+			"yield": 0,
+			"yieldType": "",
+			"colorize": [
+				{
+					x: 8,
+					y: 12,
+					len: 7
+				},
+				{
+					x: 8,
+					y: 13,
+					len: 7
+				}
+			]
+		},
 	]
 }
